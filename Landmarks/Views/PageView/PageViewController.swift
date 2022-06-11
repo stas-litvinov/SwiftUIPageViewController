15b5de1c3ac2b759fc92d604b5143e9f3816b1f8
slitvinov-promwad <Stas.Litvinov@promwad.com>
2022-06-11 22:07:21 +0600

    add navigation between pages

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -24,6 +24,8 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable  {
             navigationOrientation: .horizontal
         )
 
+        pageViewController.dataSource = context.coordinator
+
         return pageViewController
     }
 
@@ -35,13 +37,41 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable  {
         )
     }
 
-    class Coordinator: NSObject {
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
         var parent: PageViewController
         var controllers = [UIViewController]()
 
         init(_ pageViewController: PageViewController) {
             parent = pageViewController
             controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+                guard let index = controllers.firstIndex(of: viewController) else {
+                    return nil
+                }
+
+                if index == 0 {
+                    return controllers.last
+                }
+
+                return controllers[index - 1]
+        }
+
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+                guard let index = controllers.firstIndex(of: viewController) else {
+                    return nil
+                }
+
+                if index + 1 == controllers.count {
+                    return controllers.first
+                }
+
+                return controllers[index + 1]
         }
     }
 }
